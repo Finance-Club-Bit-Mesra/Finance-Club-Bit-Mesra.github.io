fa0b788b55ba60b373baf65b1846804b5ac619ae
robinaalok <btech10697.21@bitmesra.ac.in>
2022-10-26 19:53:33 +0530

    update testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,6 +266,23 @@ function lightbox_close() {
   /**
    * Testimonials slider
    */
+   const parentContainer =  document.querySelector('.swiper-wrapper');
+
+   parentContainer.addEventListener('click', event=>{
+   
+       const current = event.target;
+   
+       const isReadMoreBtn = current.className.includes('read-more-btn');
+   
+       if(!isReadMoreBtn) return;
+   
+       const currentText = event.target.parentNode.querySelector('.read-more-text');
+   
+       currentText.classList.toggle('read-more-text--show');
+   
+       current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+   
+   })
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
